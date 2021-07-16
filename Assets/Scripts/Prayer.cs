9d4e983596d17fb5afb5c37751ab2ea5c33ee0ff
TakayukiMatayoshi <c20027@std.it-college.ac.jp>
2021-07-16 16:57:19 +0900

    playerスクリプトの作成

---
+++ Assets/Scripts/Prayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prayer : MonoBehaviour
+{
+    [SerializeField] int player_maxcount;
+    int player_count;
+    bool player_alive;
+
+    void Start()
+    {
+        StageReset();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Countcheak()
+    {
+        if (player_maxcount > 0) player_maxcount--;
+        else player_alive = false;
+    }
+
+    public void StageReset()
+    {
+        player_alive = true;
+        player_count = player_maxcount;
+    }
+}

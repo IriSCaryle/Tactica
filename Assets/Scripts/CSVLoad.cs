beb461ad2cae55f8e8f3e800e1d7a339aedcad99
iriSVakjyrie <seiryuu2772@gmail.com>
2021-07-17 20:32:19 +0900

    CSVとCSVを追加するスクリプトを追加

---
+++ Assets/Scripts/CSVLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class CSVLoad : MonoBehaviour
+{
+
+    List<string[]> CSVList = new List<string[]>(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadCSV();
+    }
+
+
+    void LoadCSV()
+    {
+        TextAsset BlockCSV = Resources.Load<TextAsset>("BlockList");
+        StringReader render = new StringReader(BlockCSV.text);
+        while (render.Peek() > -1)
+        {
+            CSVList.Add(render.ReadLine().Split(','));
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

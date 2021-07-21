b01c0ede4e66e9435330ac3b23775bb22df780db
TakayukiMatayoshi <c20027@std.it-college.ac.jp>
2021-07-21 12:46:20 +0900

    ステージの配列を割り当てるスクリプト更新

---
+++ Assets/Scripts/Game/Stagemanager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Stagemanager : MonoBehaviour
+{
+    Gamemanager gamemanager;
+
+    public int vertical;
+    public int horizontal;
+
+    void Awake()
+    {
+        gamemanager = GameObject.FindGameObjectWithTag("Gamemanager").GetComponent<Gamemanager>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void oncrick()
+    {
+        Debug.Log("!");
+
+
+    }
+}

f7b172f5d3aacaf1cfa964b10981535eba169c6c
iriSVakjyrie <seiryuu2772@gmail.com>
2021-07-21 12:52:41 +0900

    カメラの移動の実装,EventTriggerのグリッドを作成

--- Assets/Scripts/Edit/MoveScreen.cs
+++ Assets/Scripts/Edit/MoveScreen.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MoveScreen : MonoBehaviour
 {
     Vector2 touchpos;
-    Vector2 startpos;
+    Vector3 startpos;
     Collider2D collider2D;
 
     bool scrollStartFlg = false;
-    const float SCROLL_END_LEFT = -2.5f;
-    const float SCROLL_END_RIGHT = 2.5f;
-    const float SCROLL_DISTANCE_CORRECTION =0.8f;
+
+    bool isFreeLook =false;
+
+    [SerializeField] Toggle toggle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,18 +23,36 @@ public class MoveScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(1) )//&& isFreeLook)
         {
+            
             touchpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             collider2D = Physics2D.OverlapPoint(touchpos);
             Debug.Log(touchpos);
-            if(scrollStartFlg == false && collider2D)
+            if (scrollStartFlg == false && collider2D)
             {
                 GameObject obj = collider2D.transform.gameObject;
                 Debug.Log(obj.name);
-
+                startpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                scrollStartFlg = true;
             }
-          
+            else
+            {
+                Vector3 diff = startpos - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                this.transform.position += diff;
+            }
+        }  
+    }
+    public void OnClickFreeLook()
+    {
+        if (toggle.isOn)
+        {
+            isFreeLook = true;
+        }
+        else
+        {
+            isFreeLook =false;
         }
     }
 }

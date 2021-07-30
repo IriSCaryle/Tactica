527c32818be0d5861a8fbaa4e273e22a937bf9e5
zel01 <c20009@std.it-college.ac.jp>
2021-07-30 13:37:10 +0900

    タイトルからの遷移完成（演出は後）

---
+++ Assets/Scripts/Title/CangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CangeScene : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickOptionButton()
+    {
+        SceneManager.LoadScene("Option");
+    }
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("ModeSelect");
+    }
+    
+}

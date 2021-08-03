c731996b664cbcedbc5a1a1418a4a1e04fafe6d6
zel01 <c20009@std.it-college.ac.jp>
2021-08-03 09:45:31 +0900

    画面遷移スクリプト更新＆オプション消去

--- Assets/Scripts/Title/CangeScene.cs
+++ Assets/Scripts/Title/CangeScene.cs
@@ -5,21 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class CangeScene : MonoBehaviour
 {
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void OnClickOptionButton()
-    {
-        SceneManager.LoadScene("Option");
-    }
     public void OnClickStartButton()
     {
         SceneManager.LoadScene("ModeSelect");

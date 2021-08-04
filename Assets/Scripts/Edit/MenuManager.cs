2627fe30ac96391dda044011a1b8e9df32e964ce
iriSVakjyrie <seiryuu2772@gmail.com>
2021-08-04 11:52:33 +0900

    UIをすべて追加

---
+++ Assets/Scripts/Edit/MenuManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MenuManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject Menu;
+    [SerializeField] Toggle MenuToggle;
+    [SerializeField] GameObject FileSetting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnClickMenu()
+    {
+        switch (MenuToggle.isOn)
+        {
+            case true:
+                Menu.SetActive(true);
+                break;
+            case false:
+                Menu.SetActive(false);
+                break;
+        }
+    }
+    public void OnCloseMenu()
+    {
+        Menu.SetActive(false);
+        MenuToggle.isOn = false;
+    }
+
+    public void OnClickFileSetting()
+    {
+        FileSetting.SetActive(true);
+    }
+    public void OnClickCloseFileSetting()
+    {
+        FileSetting.SetActive(false);
+    }
+}
